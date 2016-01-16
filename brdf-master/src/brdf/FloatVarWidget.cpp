c6c590cc1bc40a89bbb2196a0a5d67e74dd2afa6
Oleksandr Sotnychenko <sotnychenko@cg.uni-saarland.de>
2016-01-16 15:49:22 +0100

    edit the displaying of values for slidebars

--- brdf-master/src/brdf/FloatVarWidget.cpp
+++ brdf-master/src/brdf/FloatVarWidget.cpp
@@ -141,7 +141,10 @@ int FloatVarWidget::floatToSlider( float x )
 
 void FloatVarWidget::setValue( float val )
 {
-    edit->setText( QString().setNum( val ) );
+   if(abs(val-1.0)<0.01)edit->setText(QString::number(val,'g',1));
+   else
+    edit->setText( QString::number( val,'f',1 ) );
+    
    textChanged();
 }
 
